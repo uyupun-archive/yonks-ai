f10522a205d4f0462d84b3cfc4e8eecbde5aa565
Yuki Tanaka <30511546+ty9029@users.noreply.github.com>
2021-11-05 00:08:23 +0900

    Add sample

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # yonks-ai
+
+# Sample Param
+{
+  "user": {
+    "user_id": 0,
+    "feature": [
+      0, 0, 0
+    ]
+  },
+  "friends": [
+    {
+      "user_id": 1,
+      "feature": [
+        0, 1, 1
+      ]
+    },
+    {
+      "user_id": 2,
+      "feature": [
+        1, 1, 1
+      ]
+    },
+    {
+      "user_id": 3,
+      "feature": [
+        0, 0, 0
+      ]
+    }
+  ]
+}
---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
+import logging
+
+
+def euclidean_distance(x, y):
+    return np.linalg.norm((x[None, :] - y), axis=-1)
+
+
+class Feature(BaseModel):
+    user_id: int
+    feature: List[int]
+
+
+class User(BaseModel):
+    user: Feature
+    friends: List[Feature]
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/euclidean/")
+def read_item(features: User):
+    user = np.array(features.user.feature)
+    friends = np.array([f.feature for f in features.friends])
+    dist = euclidean_distance(user, friends).argsort().tolist()
+    logger = logging.getLogger('uvicorn')
+    logger.info(dist)
+
+    friends_id = [f.user_id for f in features.friends]
+    result = dict(zip(friends_id, dist))
+    logger.info(result)
+
+    return result
